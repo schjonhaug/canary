1526561290592b28f88941c800e17c5016408c8d
Andreas Schjønhaug <andreas@schjonhaug.com>
2025-06-27 09:18:01 +0200

    Connect to DB and Electrum

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,19 @@
 use sqlx::{PgPool, Row};
 
 mod electrum;
+use electrum::ElectrumClient;
 
 #[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let database_url = std::env::var("DATABASE_URL")
         .unwrap_or_else(|_| "postgresql://schjonhaug@localhost:5432/txray".to_string());
     
-    let pool = PgPool::connect(&database_url).await?;
-    
+    let _pool = PgPool::connect(&database_url).await?;
     println!("Connected to PostgreSQL database!");
+    
+    let mut electrum_client = ElectrumClient::new_regtest()?;
+    let features = electrum_client.server_features()?;
+    println!("Connected to Electrum server: {}", features);
     
     Ok(())
 }
