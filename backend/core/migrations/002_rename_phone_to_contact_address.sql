ab1d62a11ae98d2d1841cd94a6a57cd2ee2aaf2c
Andreas Schjønhaug <andreas@schjonhaug.com>
2025-07-31 13:17:37 +0200

    Initial schema

--- backend/core/migrations/002_rename_phone_to_contact_address.sql
+++
@@ -1,4 +0,0 @@
--- Migration to rename phone_number to contact_address
--- This makes the field more generic for different notification providers
-
-ALTER TABLE contact_persons RENAME COLUMN phone_number TO contact_address;
